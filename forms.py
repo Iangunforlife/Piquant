01bd181aab61759d41036ef96af939afa1f45ebe
Iangunforlife <51304174+Iangunforlife@users.noreply.github.com>
2021-06-15 22:46:56 +0800

    Migrated To SQL Database (Except Menu And Menu)

--- forms.py
+++ forms.py
@@ -1,23 +1,17 @@
-from wtforms import Form, StringField, SelectField, TextAreaField, PasswordField, validators, BooleanField
+from wtforms import Form, StringField, SelectField, TextAreaField, PasswordField, validators, BooleanField, DateField
 from wtforms.validators import email
 
 class ReservationForm(Form):
-    first_name = StringField('First Name', [validators.Length(min=1, max=150), validators.DataRequired()])
-    last_name = StringField('Last Name', [validators.Length(min=1, max=150), validators.DataRequired()])
-    salutation = SelectField('Salutation', [validators.DataRequired()], choices=[('', 'Select'), ('Dr', 'Dr'), ('Mr', 'Mr'), ('Mrs', 'Mrs'), ('Mdm', 'Mdm')], default='')
+    full_name = StringField('Full Name', [validators.Length(min=2, max=20), validators.DataRequired()])
     email = StringField('Email', [validators.DataRequired(), validators.Email()])
-    Additional_note = TextAreaField('Additional note', [validators.Optional()])
-    phone_number = StringField('Phone Number', [validators.Length(min=1, max=150), validators.DataRequired()])
-    date = StringField('Date(00/00/00)', [validators.Length(min=1, max=6), validators.DataRequired()])
-    time = StringField('Time(0000)', [validators.Length(min=1, max=4), validators.DataRequired()])
-    full_name = StringField('Full name', [validators.Length(min=1, max=150), validators.DataRequired()])
+    phone_number = StringField('Phone Number', [validators.Length(min=8, max=8), validators.DataRequired()])
+    date = DateField('Date(YYYY-MM-DD)', [validators.DataRequired()], format='%Y-%m-%d')
+    time = StringField('Time(00:00:00)', [validators.Length(min=1, max=8), validators.DataRequired()])
+    card_name = StringField('Card Holder name', [validators.Length(min=1, max=50), validators.DataRequired()])
     cn =StringField('Card Number', [validators.Length(min=16, max=16), validators.DataRequired()])
-    expire = StringField('Expiry date of card', [validators.Length(min=1, max=4), validators.DataRequired()])
+    expire = StringField('Expiry date of card YYYY-MM', [validators.Length(min=7, max=7), validators.DataRequired()])
     cvv =StringField('CVV', [validators.Length(min=1, max=3), validators.DataRequired()])
-
-class stafflogin(Form):
-    staff_id = StringField('Staff ID', [validators.Length(min=1, max=150), validators.DataRequired()])
-    password = PasswordField('Password', [validators.Length(min=1, max=150), validators.DataRequired()])
+    Additional_note = TextAreaField('Additional note', [validators.Optional()])
 
 # Akif
 class CreateUserForm(Form):
@@ -26,16 +20,30 @@ class CreateUserForm(Form):
     email = StringField('Email', [email(), validators.DataRequired()],
                         render_kw={"placeholder": "123@email.com"})
     password = PasswordField('New Password', [validators.DataRequired()], render_kw={"placeholder": "New Password"})
-    sign_up_date = StringField('Sign Up Date', [validators.Length(min=1, max=150), validators.DataRequired()],
-                               render_kw={"placeholder": "DD/MM/YYYY"})
+    phone_number = StringField('Phone_Number', [validators.Length(min=1, max=8), validators.DataRequired()],
+                               render_kw={"placeholder": "Phone Number"})
 
-class UpdatememberForm(Form):
+class UpdatememberdetailForm(Form):
     full_name = StringField('Full Name', [validators.Length(min=2, max=20), validators.DataRequired()],
                             render_kw={"placeholder": "Full Name"})
     email = StringField('Email', [email(), validators.DataRequired()],
                         render_kw={"placeholder": "123@email.com"})
-    oldpassword = PasswordField('Old Password', [validators.DataRequired()], render_kw={"placeholder": "New Password"})
-    newpassword = PasswordField('New Password', [validators.DataRequired()], render_kw={"placeholder": "New Password"})
+    phone_number = StringField('Phone_Number', [validators.Length(min=1, max=8), validators.DataRequired()],
+                               render_kw={"placeholder": "Phone Number"})
+
+class UpdatememberdetailstaffForm(Form):
+    full_name = StringField('Full Name', [validators.Length(min=2, max=20), validators.DataRequired()],
+                            render_kw={"placeholder": "Full Name"})
+    email = StringField('Email', [email(), validators.DataRequired()],
+                        render_kw={"placeholder": "123@email.com"})
+    phone_number = StringField('Phone_Number', [validators.Length(min=1, max=8), validators.DataRequired()],
+                               render_kw={"placeholder": "Phone Number"})
+    signup_date = DateField('Sign Up Date(YYYY-MM-DD)', [validators.DataRequired()])
+
+class ChangePasswordForm(Form):
+    oldpassword = PasswordField('Old Password', [validators.DataRequired()], render_kw={"placeholder": "Old Password"})
+    newpassword = PasswordField('New Password', [validators.DataRequired(), validators.EqualTo('cfmnewpassword', message='Passwords must match')], render_kw={"placeholder": "New Password"})
+    cfmnewpassword = PasswordField('Confirm New Password', [validators.DataRequired()], render_kw={"placeholder": "Confirm New Password"})
 
 class LoginForm(Form):
     email = StringField('Email', [email(), validators.DataRequired()],
@@ -48,3 +56,28 @@ class ClaimCode(Form):
 
 class CreateCode(Form):
     code = StringField('Enter New Loyalty code', [validators.optional(), validators.Length(min=6, max=20)])
+
+class CreateStaff(Form):
+    full_name = StringField('Full Name', [validators.Length(min=2, max=20), validators.DataRequired()],
+                            render_kw={"placeholder": "Full Name"})
+    email = StringField('Email', [email(), validators.DataRequired()],
+                        render_kw={"placeholder": "123@email.com"})
+    password = PasswordField('New Password', [validators.DataRequired()], render_kw={"placeholder": "New Password"})
+    phone_number = StringField('Phone_Number', [validators.Length(min=1, max=8), validators.DataRequired()],
+                               render_kw={"placeholder": "Phone Number"})
+    staff_id = StringField('Staff ID', [validators.Length(min=1, max=7), validators.DataRequired()],
+                               render_kw={"placeholder": "Staff ID"})
+    job_title = StringField('Job Title', [validators.Length(min=1, max=60), validators.DataRequired()],
+                               render_kw={"placeholder": "Job Title"})
+
+class UpdateStaff(Form):
+    full_name = StringField('Full Name', [validators.Length(min=2, max=20), validators.DataRequired()],
+                            render_kw={"placeholder": "Full Name"})
+    email = StringField('Email', [email(), validators.DataRequired()],
+                        render_kw={"placeholder": "123@email.com"})
+    phone_number = StringField('Phone_Number', [validators.Length(min=1, max=8), validators.DataRequired()],
+                               render_kw={"placeholder": "Phone Number"})
+    staff_id = StringField('Staff ID', [validators.Length(min=1, max=7), validators.DataRequired()],
+                               render_kw={"placeholder": "Staff ID"})
+    job_title = StringField('Job Title', [validators.Length(min=1, max=60), validators.DataRequired()],
+                               render_kw={"placeholder": "Job Title"})
