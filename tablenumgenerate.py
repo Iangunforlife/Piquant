410045397f18900c82f1824f49eeda036d435e59
Iangunforlife <51304174+Iangunforlife@users.noreply.github.com>
2021-06-16 18:47:20 +0800

    Delete tablenumgenerate.py

--- tablenumgenerate.py
+++
@@ -1,9 +0,0 @@
-class tablenumgen():
-    def __init__(self):
-        self.tablenum = 1
-
-    def get_tablenum(self):
-        return self.tablenum
-
-    def set_tablenum(self, tablenum):
-        self.tablenum = tablenum
